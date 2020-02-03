0d92ca300d9d98d517ed4f497bc5db4872bdbc42
pehor9164 <pehor9164@7HMN6Y1.Student.UGDSB.ED>
2020-02-03 13:19:49 -0500

    First commit test of 4U.

---
+++ src/first_test/First_Test.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package first_test;
+
+/**
+ *
+ * @author pehor9164
+ */
+public class First_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int k = 1;
+       
+        do{
+        System.out.println("Hello World");
+            System.out.println(k ++);
+        }while(k >= 1);
+    }
+    
+}
